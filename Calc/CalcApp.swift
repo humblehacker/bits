52c2a9bd7a1afdf2714683dbecf08f0f6cc588eb
David Whetstone <david@humblehacker.com>
2024-01-05 11:19:05 -0800

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by David Whetstone on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
