474907639528e7b9ea431d2ae19b1b5432ca7015
David Whetstone <david@humblehacker.com>
2024-01-31 15:40:19 -0800

    test: disable ContentView when testing

--- App/HexCalcApp.swift
+++ App/HexCalcApp.swift
@@ -1,14 +1,17 @@
 import ComposableArchitecture
 import ContentFeature
 import SwiftUI
+import XCTestDynamicOverlay
 
 @main
 struct HexCalcApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(store: Store(initialState: ContentReducer.State()) {
-                ContentReducer()
-            })
+            if !_XCTIsTesting {
+                ContentView(store: Store(initialState: ContentReducer.State()) {
+                    ContentReducer()
+                })
+            }
         }
         .windowStyle(.automatic)
         .windowToolbarStyle(.unified)
